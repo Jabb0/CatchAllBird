a5bd70b4cf54d6a596c9739b392a5dc4527a537e
Jabb0 <33359018+Jabb0@users.noreply.github.com>
2024-01-07 20:38:40 +0100

    fixed import

--- src/background/background.js
+++ src/background/background.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/ 
  */
 
-import { processInbox, processMessagesInFolder, onNewMailReceived} from "./processing";
+import { processInbox, processMessagesInFolder, onNewMailReceived} from "./processing.js";
 
 async function welcomeTab() {
     await messenger.tabs.create({
